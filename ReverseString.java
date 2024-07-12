d069507230435d1bc4d86235ddfde7a5d9d88e01
Ajani Lewis <alewis@griddynamics.com>
2024-07-11 19:54:24 -0500

    write implementation for task 2

---
+++ ReverseString.java
@@ -0,0 +1,41 @@
+public class ReverseString {
+
+    
+    public static String firstImplementation(String word){
+        int wordLength;
+
+        wordLength = word.length();
+
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = wordLength - 1; i >= 0; i--){
+            reversed.append(word.charAt(i));
+        }
+
+        return reversed.toString();
+    }
+
+    public static String secondImplementation(String word){
+        StringBuffer reversed = new StringBuffer(word);
+        return reversed.reverse().toString();
+    }
+
+    public static String thirdImplementation(String word){
+        if (word.isEmpty()){
+            return word;
+        }
+        return thirdImplementation(word.substring(1)) + word.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        String first, second, third;
+        first = firstImplementation(args[0]);
+        second = secondImplementation(args[0]);
+        third = thirdImplementation(args[0]);
+
+        System.out.println("###RESULTS###");
+        System.out.println("1st Implementation (for loop): " + first);
+        System.out.println("2nd Implementation (string buffer reverse function): " + second);
+        System.out.println("3rd Implementation (recursion): " + third);
+    }
+}
